b48dbbe022809cfdb14a553a20a985227077044c
Bradley van Ewijk <527644@student.glu.nl>
2018-09-26 10:57:18 +0200

    Basic Player Controller
    It works

---
+++ glupartygame/Assets/Minigames/StoelenDans/Scripts/Bradley/Player_Controller.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Controller : MonoBehaviour
+{
+	void Update ()
+    {
+        var x = Input.GetAxis("Key_Horizontal") * Time.deltaTime * 150.0f;
+        var z = Input.GetAxis("Key_Vertical") * Time.deltaTime * 3.0f;
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+    }
+}
