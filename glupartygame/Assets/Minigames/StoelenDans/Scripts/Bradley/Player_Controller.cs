3b4eba6884c25855a04cc07d9e285629f646722a
Bradley van Ewijk <527644@student.glu.nl>
2018-10-17 09:06:27 +0200

    Removal
    Removed Obi folder and fixed player

--- glupartygame/Assets/Minigames/StoelenDans/Scripts/Bradley/Player_Controller.cs
+++ glupartygame/Assets/Minigames/StoelenDans/Scripts/Bradley/Player_Controller.cs
@@ -4,19 +4,6 @@ using UnityEngine;
 
 namespace StoelenDans
 {
-<<<<<<< HEAD
-
-	void Start ()
-    {
-		
-	}
-	
-	void Update ()
-    {
-		
-	}
-}
-=======
     public class Player_Controller : MonoBehaviour
     {
         [SerializeField]
@@ -85,6 +72,4 @@ namespace StoelenDans
             }
         }
     }
-
 }
->>>>>>> Bradley
