516038a0b71e62cb120f560db990253d67c49c39
Wallykiller1 <waldo_vandijk@hotmail.com>
2018-10-17 08:38:13 +0200

    scenes+scripts+assets
    v

---
+++ glupartygame/Assets/Minigames/StoelenDans/Scripts/Waldo/TapPad.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace StoelenDans
+{
+    public class TapPad : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+        void OnTriggerStay(Collider Player)
+        {
+            PlayerGlass _Glass = Player.GetComponent<PlayerGlass>();
+
+            if(_Glass != null)
+            {
+                Player.gameObject.transform.position = this.gameObject.transform.position;
+            }
+        }
+    }
+}
+
