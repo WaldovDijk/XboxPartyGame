efb882ee7fceb8e18033a87b803fe222d2b43e31
Bradley van Ewijk <527644@student.glu.nl>
2018-10-30 14:32:03 +0100

    PauseScript.
    Added pause script 90% done!

--- glupartygame/Assets/Minigames/StoelenDans/Scripts/Bradley/PauseManager.cs
+++ glupartygame/Assets/Minigames/StoelenDans/Scripts/Bradley/PauseManager.cs
@@ -2,15 +2,102 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PauseManager : MonoBehaviour {
+namespace StoelenDans
+{
+    public class PauseManager : MonoBehaviour
+    {
+        public static PauseManager Instance;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        private List<Rigidbody> m_Rigidbodies;
+        private List<ParticleSystem> m_ParticleSystems;
+
+        private List<RigidbodyData> m_Data;
+
+        public bool IsPaused;
+
+        private void Awake()
+        {
+            Instance = this;
+            m_Rigidbodies = new List<Rigidbody>();
+            m_Data = new List<RigidbodyData>();
+        }
+
+        public void Add(Rigidbody _rigidbody)
+        {
+            m_Rigidbodies.Add(_rigidbody);
+        }
+
+        public void Add(ParticleSystem _ps)
+        {
+            m_ParticleSystems.Add(_ps);
+        }
+
+        public void Update()
+        {
+            if (Input.GetKey(KeyCode.P))
+            {
+                Pause();
+            }
+
+            if (Input.GetKey(KeyCode.O))
+            {
+                Resume();
+            }
+        }
+
+        public void Pause()
+        {
+            m_Data.Clear();
+
+            for (int i = 0; i < m_Rigidbodies.Count; i++)
+            {
+                m_Data.Add(new RigidbodyData()
+                {
+                    InstanceID = m_Rigidbodies[i].GetInstanceID(),
+                    Velocity = m_Rigidbodies[i].velocity,
+                    Drag = m_Rigidbodies[i].drag,
+                    Gravity = m_Rigidbodies[i].useGravity,
+                    Kinematic = m_Rigidbodies[i].isKinematic
+                });
+
+                m_Rigidbodies[i].isKinematic = true;
+                m_Rigidbodies[i].useGravity = false;
+
+                m_Rigidbodies[i].velocity = Vector3.zero;
+                m_Rigidbodies[i].drag = 0;
+
+                m_Rigidbodies[i].Sleep();
+            }
+
+            IsPaused = true;
+        }
+
+        public void Resume()
+        {
+            for (int i = 0; i < m_Rigidbodies.Count; i++)
+            {
+                RigidbodyData _data = m_Data.Find(j => j.InstanceID == m_Rigidbodies[i].GetInstanceID());
+
+                m_Rigidbodies[i].isKinematic = _data.Kinematic;
+                m_Rigidbodies[i].useGravity = _data.Gravity;
+
+                m_Rigidbodies[i].velocity = _data.Velocity;
+                m_Rigidbodies[i].drag = _data.Drag;
+
+                m_Rigidbodies[i].WakeUp();
+            }
+
+            IsPaused = false;
+        }
+
+        private struct RigidbodyData
+        {
+            public int InstanceID;
+
+            public bool Kinematic;
+            public float Drag;
+            public bool Gravity;
+            public Vector3 Velocity;
+        }
+    }
 }
